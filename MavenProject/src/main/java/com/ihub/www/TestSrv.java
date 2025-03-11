fb298b155554d3a645bb79863e0268dbdddfadee
Vincent Nathaniel <puppyavn334@gmail.com>
2025-03-11 16:34:57 +0530

    my maven proj

---
+++ MavenProject/src/main/java/com/ihub/www/TestSrv.java
@@ -0,0 +1,23 @@
+package com.ihub.www;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class TestSrv extends HttpServlet
+{
+	protected void doGet(HttpServletRequest req,HttpServletResponse res)throws ServletException,IOException
+	{
+		PrintWriter pw=res.getWriter();
+		res.setContentType("text/html");
+		
+		pw.println("<center><h1>Servlet is called </h1></center>");
+		
+		pw.close();
+	}
+}
+
---
+++ MavenProject/src/main/webapp/index.jsp
@@ -0,0 +1,5 @@
+<center>
+	<h1>
+		<a href="test"> Click Me</a>
+	</h1>
+</center>
